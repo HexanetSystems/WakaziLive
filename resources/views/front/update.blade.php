ab5355cfdef690c6efa547ffefc1627bb799bc1b
AlbertWebs <albertmuhatia@gmail.com>
2024-07-10 21:14:35 +0300

    Update

--- resources/views/front/update.blade.php
+++ resources/views/front/update.blade.php
@@ -42,66 +42,9 @@
                         </div>
                         <div class="dz-info">
                             <div class="dz-post-text">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                                <blockquote class="wp-block-quote is-style-default"><p>Create An Information Architecture That’s Easy To Use Way Precise Usability Considerations For Partially </p><cite>Ronald M. Spino</cite><i class="flaticon-right-quote"></i></blockquote>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                                {{-- <figure class="wp-container-5 wp-block-gallery-3 wp-block-gallery has-nested-images columns-3 is-cropped">
-                                    <figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic1.jpg" alt="/"></figure>
-                                    <figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic2.jpg" alt="/"></figure>
-                                    <figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic3.jpg" alt="/"></figure>
-                                    <figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic4.jpg" alt="/"></figure>
-                                    <figure class="wp-block-image size-large"><img src="images/blog/blogpost-3/pic1.jpg" alt="/"></figure>
-                                </figure> --}}
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-                                <h3 class="dz-title">Additional information</h3>
-                                <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-                                <ul class="list-check-2 check-circle">
-                                    <li>Sustainable Materials and Eco-Friendly Fabrics</li>
-                                    <li>Promoting Fair Labor Practices in Fashion</li>
-                                    <li>The Three R's in Sustainable Fashion</li>
-                                    <li>Capsule Wardrobes Curating Timeless Style</li>
-                                    <li>Transparency in Fashion Supply Chains</li>
-                                    <li>Vegan and Cruelty-Free Fashion Alternatives</li>
-                                </ul>
-
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                <p>{!!html_entity_decode($blog->content)!!}</p>
                             </div>
-                            <div class="dz-share-post meta-bottom">
-                                <div class="post-tags">
-                                    <strong>Tags:</strong>
-                                    <a href="javascript:void(0);">Dresses</a>
-                                    <a href="javascript:void(0);">Boots</a>
-                                    <a href="javascript:void(0);">Jewelry</a>
-                                    <a href="javascript:void(0);">Jacket</a>
-                                    <a href="javascript:void(0);">Coat</a>
-                                </div>
-                                <div class="dz-social-icon primary-light">
-                                    <ul>
-                                        <li>
-                                            <a href="https://www.facebook.com/dexignzone">
-                                                <i class="fab fa-facebook-f"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.instagram.com/dexignzone/">
-                                                <i class="fab fa-instagram"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://twitter.com/dexignzones">
-                                                <i class="fab fa-twitter"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.linkedin.com/showcase/3686700/admin/">
-                                                <i class="fa-brands fa-linkedin-in"></i>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+
                         </div>
 
                     </div>
