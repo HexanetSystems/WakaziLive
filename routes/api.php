61d832857b120648ddf2a20e82bafb0fbab7a35c
AlbertWebs <albertmuhatia@gmail.com>
2024-07-25 17:35:46 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Auth::routes();
 Route::post('/stk-callback', [App\Http\Controllers\KcbController::class, 'stkCallback'])->name('stk-callback');
-
