887256e89e2ac126d863a8800cecfc8f8bd91982
AlbertWebs <albertmuhatia@gmail.com>
2024-08-16 10:59:14 +0300

    update

--- resources/views/front/intagram-feeds.blade.php
+++ resources/views/front/intagram-feeds.blade.php
@@ -6,13 +6,13 @@
 
                         <?php $counter = 1; ?>
                         @foreach($data as $item)
-                            @if($item['type'] == 'video')
+                            @if($item->type == 'video')
 
                             @else
                             <div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-4 wow fadeIn" data-wow-delay="0.1s">
                                 <div class="insta-post dz-media dz-img-effect rotate">
-                                    <a target="new" href="{{$item['permalink']}}">
-                                        <img src="{{$item['url']}}" alt="">
+                                    <a target="new" href="{{$item->permalink}}">
+                                        <img src="{{$item->url}}" alt="">
                                     </a>
                                 </div>
                             </div>
