7e5b316874c6b95d23503050c5ae51ad0d888382
AlbertWebs <albertmuhatia@gmail.com>
2024-08-16 06:39:55 +0300

    Update

--- resources/views/front/intagram-feeds.blade.php
+++ resources/views/front/intagram-feeds.blade.php
@@ -5,42 +5,42 @@
 				<div class="row gx-0">
 					<div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-4 wow fadeIn" data-wow-delay="0.1s">
 						<div class="insta-post dz-media dz-img-effect rotate">
-							<a href="portfolio-tiles.html">
+							<a href="#">
 								<img src="{{url('/')}}/uploads/instagram/Earthenware 2.webp" alt="">
 							</a>
 						</div>
 					</div>
 					<div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-4 wow fadeIn" data-wow-delay="0.2s">
 						<div class="insta-post dz-media dz-img-effect rotate">
-							<a href="portfolio-tiles.html">
+							<a href="#">
 								<img src="{{url('/')}}/uploads/instagram/Earthenware 2.webp" alt="">
 							</a>
 						</div>
 					</div>
 					<div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-4 wow fadeIn" data-wow-delay="0.3s">
 						<div class="insta-post dz-media dz-img-effect rotate">
-							<a href="portfolio-tiles.html">
+							<a href="#">
 								<img src="{{url('/')}}/uploads/instagram/Earthenware 2.webp" alt="">
 							</a>
 						</div>
 					</div>
 					<div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-4 wow fadeIn" data-wow-delay="0.4s">
 						<div class="insta-post dz-media dz-img-effect rotate">
-							<a href="portfolio-tiles.html">
+							<a href="#">
 								<img src="{{url('/')}}/uploads/instagram/Earthenware 2.webp" alt="">
-							</a>	
+							</a>
 						</div>
 					</div>
 					<div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-4 wow fadeIn" data-wow-delay="0.5s">
 						<div class="insta-post dz-media dz-img-effect rotate">
-							<a href="portfolio-tiles.html">
+							<a href="#">
 								<img src="{{url('/')}}/uploads/instagram/Earthenware 2.webp" alt="">
 							</a>
 						</div>
 					</div>
 					<div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-4 wow fadeIn" data-wow-delay="0.6s">
 						<div class="insta-post dz-media dz-img-effect rotate">
-							<a href="portfolio-tiles.html">
+							<a href="#">
 								<img src="{{url('/')}}/uploads/instagram/Earthenware 2.webp" alt="">
 							</a>
 						</div>
